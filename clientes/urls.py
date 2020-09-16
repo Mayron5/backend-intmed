793d7d6da07818919407cabc62cbf017af240d0c
Rodrigo Cardoso <rodrigo@pop-os.localdomain>
2020-09-16 12:52:32 -0300

    Requirement | Clientes OK!

---
+++ clientes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf.urls import include, url
+
+from clientes.views import CriarCliente
+
+urlpatterns = [
+    path('criar', CriarCliente.as_view())
+]
--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import ClienteSerializer
 
-# Create your views here.
+
+
+class CriarCliente(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = ClienteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
