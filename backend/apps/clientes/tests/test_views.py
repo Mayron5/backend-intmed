0fc5b5c6393f77aecfa43c25fa13dab0aeff1ead
Rodrigo Cardoso <rodrigo@pop-os.localdomain>
2020-09-29 11:11:38 -0300

    Tests | Cliente OK!

---
+++ backend/apps/clientes/tests/test_views.py
@@ -0,0 +1,26 @@
+from rest_framework import status
+from .test_setup import TestSetUp
+from ..models import Cliente
+
+
+class TestViews(TestSetUp):
+    def test_registrar_cliente_sem_dados(self):
+        response = self.client.post(self.register_url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_nao_verificado(self):
+        self.client.post(self.register_url, self.user_data, format='json')
+        response = self.client.post(self.login_url, self.user_data,
+                                    format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_cliente_registrado_corretamente(self):
+        response = self.client.post(self.register_url,
+                                    self.user_data, format='json')
+
+        self.assertEqual(response.data['email'], self.user_data['email'])
+        self.assertEqual(response.data['username'], self.user_data['username'])
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
--- backend/apps/clientes/urls.py
+++ backend/apps/clientes/urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from backend.apps.clientes.views import criar_cliente
 
 urlpatterns = [
-    path('api', criar_cliente),
+    path('api/register', criar_cliente, name='register'),
     path('api/login', obtain_auth_token, name="login")
 ]
